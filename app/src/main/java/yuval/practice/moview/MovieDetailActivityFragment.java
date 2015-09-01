6b1337978c5ee19610fc4db2e03e38838ccbec16
Yuval Wyborski <yvw.bor@gmail.com>
2015-09-01 08:43:41 +0300

    Add MovieDetail activity and fragment.

---
+++ app/src/main/java/yuval/practice/moview/MovieDetailActivityFragment.java
@@ -0,0 +1,31 @@
+package yuval.practice.moview;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieDetailActivityFragment extends Fragment {
+
+    public MovieDetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+
+        return rootView;
+    }
+}
